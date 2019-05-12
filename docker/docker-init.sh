8dadf9180104bec0f2d5612b91113acce038ce53
Alexandre Balon-Perin <alexbp13@gmail.com>
2019-05-13 00:09:17 +0900

    added submodules

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -1,4 +1,4 @@
-docker build -t cogentlabs/api-gateway ../api-gateway/
-docker build -t cogentlabs/image-service ../image-service/
-docker build -t cogentlabs/image-processor ../image-processor/
+docker build -t cogentlabs/api-gateway ../cogentlabs-api-gateway/
+docker build -t cogentlabs/image-service ../cogentlabs-image-service/
+docker build -t cogentlabs/image-processor ../cogentlabs-image-processor/
 docker build -t cogentlabs/rabbitmq ./rabbitmq/
